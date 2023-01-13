3ac41c911579c5c2fa32d1465f2efb8694c26b0f
ziwenkan <kanziwensteven@gmail.com>
2023-01-13 14:48:12 -0800

    pic

--- reproduce.py
+++ reproduce.py
@@ -115,7 +115,8 @@ def CNN_debug():
                     window_correct_train += (predicted == labels).sum().item()
             correct_percentage_test = window_correct_test / window_total_test
             correct_percentage_train = window_correct_train / window_total_train
-
+            if epoch == EPOCH - 1 and correct_percentage_test > 0.5:
+                correct += 1
             print('%s: epoch %d train per:%.3f,test per:%.3f,run loss %.3f' % (
             number, epoch, correct_percentage_train, correct_percentage_test, running_loss))
             train_acc_epoch.append(correct_percentage_train)
@@ -145,94 +146,13 @@ def CNN_debug():
         ax.legend()
 
         plt.savefig(os.path.join('visualize/CNN_acc_loss', number))
+
+    print('total acc: %.3f' % (correct/total))
 CNN_debug()
 exit()
 
 j = 0
 
-
-def CNN_debug():
-    EPOCH = 10
-    WINDOW_SIZE = 33
-    WINDOW_STEP = 33
-    correct = 0
-    total = len(all_group_number)
-    paitent_makers, window_labels, window_data = window_oper(all_group_number, WINDOW_SIZE, WINDOW_STEP)
-    window_labels = np.array(window_labels)
-    window_data = np.array(window_data)
-    # print('current: EPOCH %d W_SIZE %d W_STEP %d' % (EPOCH, WINDOW_SIZE, WINDOW_STEP))
-    for number in all_group_number:
-        testing_idx = [i for i, x in enumerate(paitent_makers) if x == number]
-        training_idx = [i for i, x in enumerate(paitent_makers) if x != number]
-
-        testing_labels = window_labels[testing_idx]
-        testing_data = window_data[testing_idx, :]
-        training_labels = window_labels[training_idx]
-        training_data = window_data[training_idx, :]
-
-        trainset = DMDDataset(training_labels, training_data, transforms)
-        testset = DMDDataset(testing_labels, testing_data, transforms)
-
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS,
-                                                  shuffle=True)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS,
-                                                 shuffle=False)
-
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-        net = CNN_DMD(WINDOW_SIZE).float().to(device)
-        optimizer = optim.Adam(net.parameters())
-        loss_function = nn.CrossEntropyLoss()
-        # print('patient: ', number, ' train start')
-        for epoch in range(EPOCH):
-            running_loss = 0
-            for i, (sample) in enumerate(trainloader, 0):
-                # get the inputs
-
-                labels, data = sample['label'].to(device).to(torch.int64), sample['data'].to(device).float()
-
-                optimizer.zero_grad()
-                outputs = net(data)
-
-                loss = loss_function(outputs, labels)
-                loss.backward()
-                optimizer.step()
-
-                running_loss += loss.item()
-            # print(running_loss)
-            window_correct = 0
-            window_total = 0
-            train_total = 0
-            train_correct = 0
-            # print('patient: ', number, ' train finished')
-            with torch.no_grad():
-                for i, sample in enumerate(testloader, 0):
-                    labels, data = sample['label'].to(device).to(torch.int64), sample['data'].to(device).float()
-
-                    outputs = net(data)
-                    _, predicted = torch.max(outputs.data, 1)
-                    window_total += labels.size(0)
-
-                    window_correct += (predicted == labels).sum().item()
-                correct_percentage_test = window_correct / window_total
-
-                for i, sample in enumerate(trainloader, 0):
-                    labels, data = sample['label'].to(device).to(torch.int64), sample['data'].to(device).float()
-
-                    outputs = net(data)
-                    _, predicted = torch.max(outputs.data, 1)
-                    train_total += labels.size(0)
-
-                    train_correct += (predicted == labels).sum().item()
-
-                correct_percentage = train_correct / train_total
-                print('%s: train correct percentage:%.3f test correct percentage:%.3f' % (
-                number, correct_percentage, correct_percentage_test))
-        if correct_percentage_test > 0.5:
-            correct += 1
-
-    csv_writer(EPOCH, WINDOW_STEP, WINDOW_SIZE, 'bs_' + str(BATCH_SIZE) + '_lr_' + str(LEARN_RATE),
-               (correct / total))
 
 
 CNN_debug()
